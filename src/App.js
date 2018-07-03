18b7b5bec374fc735ff963f729164cf1bf62e6ce
mozhou <mozhou216@gmail.com>
2018-07-02 20:45:59 -0400

    add server

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
+
+const TodosQuery = gql`
+{
+  todos{
+    id
+    text
+    complete
+  }
+}
+`
 
 class App extends Component {
   render() {
+    console.log(this.props)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        hello
       </div>
     );
   }
 }
 
-export default App;
+export default graphql(TodosQuery)(App);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import ApolloClient from "apollo-boost";
+import {ApolloProvider} from 'react-apollo';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: "http://localhost:4000"
+});
+
+ReactDOM.render(
+  <ApolloProvider client = { client }>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
